cef80cd231d7c08102902da81b0997cb88ff9e9d
JonLePotatoPhone <jcdiem3@gmail.com>
2019-06-03 15:15:03 -0500

    Finished FOV workings -Lighted areas are now orange like a torch -Changed the player color to better see in orange, along with other entities -Old areas are the same color they were before, blue -Added entity colors to the colors list so I can more easily change them as things go along -Prepard the FOV var to be able to be changed back to base for status effect use

--- engine.py
+++ engine.py
@@ -1,6 +1,7 @@
 import tcod as tcd
 from input_handling import handle_keys
 from entity import Entity
+from fov_functions import initialize_fov, recompute_fov
 from render_functions import clear_all, render_all
 from map_objects.game_map import GameMap
 
@@ -17,19 +18,23 @@ def main():
     max_rooms = 30
 
     #FOV stuff
+    fov_radius_base = 5
     fov_algorithm = 0
     fov_light_walls = True
-    fov_radius = 10
+    fov_radius = fov_radius_base #Set to base so it can be modified by the game for torches etc
 
     colors = {
+        'player': tcd.Color(50,255,50),
+        'enemy': tcd.Color(255,50,255),
+        'friendly': tcd.Color(75,255,75),
         'dark_wall': tcd.Color(0, 0, 100),
         'dark_ground': tcd.Color(50, 50, 150),
         'light_wall': tcd.Color(130,110,50),
         'light_ground': tcd.Color(200,100,50)
     }
 
-    ply = Entity(int(screen_width / 2), int(screen_height / 2), '@', tcd.red)
-    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', tcd.yellow)
+    ply = Entity(int(screen_width / 2), int(screen_height / 2), '@', colors.get('player'))
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', colors.get('enemy'))
     entities = [npc, ply]
 
     tcd.console_set_custom_font('font.png', tcd.FONT_TYPE_GREYSCALE | tcd.FONT_LAYOUT_TCOD)
@@ -40,13 +45,23 @@ def main():
     game_map = GameMap(map_width, map_height)
     game_map.make_map(max_rooms,room_min_size,room_max_size,map_width,map_height,ply)
 
+    fov_recompute = True
+
+    fov_map = initialize_fov(game_map)
+
     key = tcd.Key()
     mouse = tcd.Mouse()
 
     while not tcd.console_is_window_closed():
         tcd.sys_check_for_event(tcd.EVENT_KEY_PRESS, key, mouse)
 
-        render_all(con, entities, game_map, screen_width, screen_height, colors)
+        if fov_recompute:
+            recompute_fov(fov_map, ply.x, ply.y, fov_radius, fov_light_walls, fov_algorithm)
+
+        render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, screen_height, colors)
+
+        fov_recompute = False 
+
         tcd.console_flush()
 
         clear_all(con, entities)
@@ -62,6 +77,8 @@ def main():
             dx, dy = move
             if not game_map.is_blocked(ply.x + dx, ply.y + dy):
                 ply.move(dx,dy)
+
+                fov_recompute = True
 
         if exit:
             return True
